8095cbf1c93827cadd4d61b91726b684fff2df08
titsuki <titsuki@cpan.org>
2015-05-31 22:42:22 +0900

    updated

--- Volume_11/1145_The_Genome_Database_of_All_Space_Life.cpp
+++ Volume_11/1145_The_Genome_Database_of_All_Space_Life.cpp
@@ -30,10 +30,57 @@ int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+struct State {
+  int pos;
+  string str_result;
+  int int_result;
+  State(int pos,string str_result) 
+    : pos(pos), str_result(str_result) {}
+  State(int pos,int int_result) 
+    : pos(pos), int_result(int_result) {}
+};
+
+// EXPR ::= NUMBERS ( EXPR ) | ALPHAS
+
+State alphas(const string& str,int pos){
+  string tmp = "";
+  while(isalpha(str[pos])){
+    tmp.push_back(str[pos]);
+    pos++;
+  }
+  return State(pos,tmp);
+}
+
+State numbers(const string& str,int pos){
+  stringstream ss;
+  while(isdigit(str[pos])){
+    ss << str[pos];
+    pos++;
+  }
+  return State(pos,atoi(ss.str().c_str()));
+}
+
+State expr(const string& str,int pos){
+  if(isdigit(str[pos])){
+    State s1 = numbers(str,pos);
+    State s2 = expr(str,s1.pos + 1);
+
+    string tmp = "";
+    for(int i = 0; i < s1.int_result; i++){
+      tmp += s2.str_result;
+    }
+    return State(s2.pos,tmp);
+  }
+  else{
+    return alphas(str,pos);
+  }
+}
+
+
 int main(){
   string genome;
   int pos;
   while(cin >> genome >> pos){
-    
+    cout << expr(genome,0).str_result << endl;
   }
 }
