af00b64e77144c60f5d230c23014844a31fb60fb
titsuki <titsuki@cpan.org>
2015-06-26 22:21:19 +0900

    MLE

--- Volume_26/2607_Invest_Master.cpp
+++ Volume_26/2607_Invest_Master.cpp
@@ -34,16 +34,52 @@ static const int ty[] = {-1,0,1,0};
 int main(){
   int num_of_stocks;
   int period;
-  int money;
+  int init_money;
   while(~scanf("%d %d %d",
 	       &num_of_stocks,
 	       &period,
-	       &money)){
+	       &init_money)){
     int price[11][11];
+    map<vector<int>,int> dp[11][11];
+
     for(int day = 0; day < period; day++){
       for(int stock_i = 0; stock_i < num_of_stocks; stock_i++){
 	scanf("%d",&price[day][stock_i]);
       }
     }
+    vector<int> init_state(10);
+    dp[0][0][init_state] = init_money;
+    for(int day = 0; day < period; day++){
+      for(int stock_i = 0; stock_i < num_of_stocks; stock_i++){
+	for(map<vector<int>,int>::iterator it = dp[day][stock_i].begin(); it != dp[day][stock_i].end(); it++){
+	  vector<int> prev_state = it->first;
+	  int prev_money = it->second;
+	  dp[day][stock_i+1][prev_state] = max(prev_money,dp[day][stock_i+1][prev_state]);
+	  
+	  if(prev_state[stock_i] > 0){
+	    vector<int> next_state = prev_state;
+	    next_state[stock_i] = 0;
+	    dp[day][stock_i+1][next_state] = max(dp[day][stock_i+1][next_state],
+						 prev_money
+						 + price[day][stock_i] * prev_state[stock_i]);
+	  }
+	  for(int buy = 1; buy <= 1000000; buy++){
+	    if(prev_money - buy * price[day][stock_i] < 0) break;
+	    vector<int> next_state = prev_state;
+	    next_state[stock_i] += buy;
+	    dp[day][stock_i+1][next_state] = max(dp[day][stock_i+1][next_state],
+						 prev_money
+						 - price[day][stock_i] * buy);
+	  }
+	}
+      }
+      dp[day+1][0] = dp[day][num_of_stocks];
+    }
+
+    int res = 0;
+    for(map<vector<int>,int>::iterator it = dp[period][0].begin(); it != dp[period][0].end(); it++){
+      res = max(it->second,res);
+    }
+    printf("%d\n",res);
   }
 }
