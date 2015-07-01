11971a1e397ed1e5f3e90d915598d70544c0e1c7
titsuki <titsuki@cpan.org>
2015-07-01 22:47:56 +0900

    added input portion

---
+++ Volume_26/2660_sigma.cpp
@@ -0,0 +1,44 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+ 
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+   
+using namespace std;
+   
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+   
+static const double EPS = 1e-8;
+   
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+int main(){
+  int N;
+  while(~scanf("%d",&N)){
+    vector<int> numbers;
+    for(int i = 0; i < N; i++){
+      int number;
+      scanf("%d",&number);
+      numbers.push_back(number);
+    }
+  }
+}
