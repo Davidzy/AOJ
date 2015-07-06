b3e40174e366123833afd07d389d1e5e98c41cf0
titsuki <titsuki@cpan.org>
2015-07-06 23:18:53 +0900

    TLE

---
+++ Volume_26/2674_Disordered_Data_Detection.cpp
@@ -0,0 +1,64 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+int main(){
+  int sequence_length;
+  while(~scanf("%d",&sequence_length)){
+    vector<int> sequence;
+    for(int seq_i = 0; seq_i < sequence_length; seq_i++){
+      int x;
+      scanf("%d",&x);
+      sequence.push_back(x);
+    }
+    int total_queries;
+    scanf("%d",&total_queries);
+    for(int query_i = 0; query_i < total_queries; query_i++){
+      int left_i;
+      int right_i;
+      int offset;
+      scanf("%d %d %d",&left_i,&right_i,&offset);
+      left_i--;
+      right_i--;
+      int max_v = max(sequence[left_i],sequence[right_i]);
+      int min_v = min(sequence[left_i],sequence[right_i]);
+      int count = 0;
+      for(int seq_i = left_i; seq_i <= right_i; seq_i++){
+	if(sequence[seq_i] < min_v - offset
+	   || max_v + offset < sequence[seq_i]){
+	  count++;
+	}
+      }
+      printf("%d\n",count);
+    }
+  }
+}
