0ea3792be6b29b7ee00a7a429e51df91b2a11362
titsuki <titsuki@cpan.org>
2015-07-30 00:06:53 +0900

    added input portion

---
+++ Volume_21/2131_Pi_is_Three.cpp
@@ -0,0 +1,43 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+bool equal(double lhs, double rhs){
+  return (lhs - EPS <= rhs && rhs - EPS <= lhs);
+}
+
+int main(){
+  double R;
+  while(cin >> R){
+    if(equal(R,0.0)) break;
+  }
+}
