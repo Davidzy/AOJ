d2e7258db7e202c9c20d3d616bb65f46e878a3be
titsuki <titsuki@cpan.org>
2015-08-20 00:46:51 +0900

    updated

--- Volume_05/0598_JOI_Emblem.cpp
+++ Volume_05/0598_JOI_Emblem.cpp
@@ -30,59 +30,65 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
-char stage[1001][1001];
-char crest[5][5];
+char stage[1010][1010];
+char crest[8][8];
 int dp[1001][1001];
+int W,H;
 
-bool match(int x,int y){
+int match(int x,int y){
   int count = 0;
-  for(int crest_y = y - 1,dy = 0; crest_y <= y; crest_y++,dy++){
-    for(int crest_x = x - 1,dx = 0; crest_x <= x; crest_x++,dx++){
-      if(stage[crest_y][crest_x] == crest[dy][dx]){
+  if(x < 0 || y < 0 || x + 1 >= W || y + 1 >= H) return 0;
+  for(int stage_y = y,crest_y = 0; stage_y <= y + 1; stage_y++,crest_y++){
+    for(int stage_x = x,crest_x = 0; stage_x <= x + 1; stage_x++,crest_x++){
+      if(stage[stage_y][stage_x] == crest[crest_y][crest_x]){
 	count++;
       }
     }
   }
-  return (count == 4);
+  return (count == 4 ? 1 : 0);
 }
 
 int main(){
-  int H,W;
   while(~scanf("%d %d",&H,&W)){
     memset(dp,0,sizeof(dp));
+    memset(stage,'\0',sizeof(stage));
+    memset(crest,'\0',sizeof(crest));
     for(int y = 0; y < H; y++){
-      char line[1001];
+      char line[1010];
       scanf("%s",line);
       for(int x = 0; x < W; x++){
 	stage[y][x] = line[x];
       }
     }
     for(int y = 0; y < 2; y++){
-      char line[3];
+      char line[4];
       scanf("%s",line);
       for(int x = 0; x < 2; x++){
 	crest[y][x] = line[x];
       }
     }
-    for(int y = 1; y < H; y++){
-      for(int x = 1; x < W; x++){
-	if(match(x,y)){
-	  for(int dy = y - 2,dx = x - 2; dy <= y && dx >= 0; dy++){
-	    dp[y][x] = max(dp[y][x],dp[dy][dx] + 1);
-	  }
-	  for(int dx =  x - 2,dy = y - 2; dx <= x && dy >= 0; dx++){
-	    dp[y][x] = max(dp[y][x],dp[dy][dx] + 1);
-	  }
-	}
+
+    int res = 0;
+
+    for(int y = 0; y < H; y++){
+      for(int x = 0; x < W; x++){
+	res += match(x,y);
       }
     }
 
-    int res = 0;
-    for(int y = 1; y < H; y++){
-      for(int x = 1; x < W; x++){
-	res = max(dp[y][x],res);
+    const char JOI[] = {'J','O','I'};
+    for(int y = 0; y < H; y++){
+      for(int x = 0; x < W; x++){
+	for(int char_i = 0; char_i < 3; char_i++){
+	  int tmp = match(x,y) + match(x-1,y) + match(x,y-1) + match(x-1,y-1);
+	  char prev = stage[y][x];
+	  stage[y][x] = JOI[char_i];
+	  res = max(res,res - tmp + (match(x,y) + match(x-1,y) + match(x,y-1) + match(x-1,y-1)));
+	  stage[y][x] = prev;
+	}
       }
     }
+    
     printf("%d\n",res);
   }
 }
