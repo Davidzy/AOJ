16c5bdd3100ec495a19606e06a79786c5f1d6cd5
titsuki <titsuki@cpan.org>
2015-09-19 02:18:47 +0900

    TLE

--- Volume_10/1023_Amazing_Graze.cpp
+++ Volume_10/1023_Amazing_Graze.cpp
@@ -27,6 +27,20 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
+class Point {
+public:
+  int x;
+  int y;
+  Point(int x,int y){
+    this->x = x;
+    this->y = y;
+  }
+  bool include(const Point& p,int radius){
+    return ((x - p.x) * (x - p.x)
+	    + (y - p.y) * (y - p.y) <= 16 * radius * radius);
+  }
+};
+
 int main(){
   int num_of_combat_planes;
   int num_of_energy_bullets;
@@ -35,13 +49,31 @@ int main(){
 	       &num_of_combat_planes,
 	       &num_of_energy_bullets,
 	       &radius)){
+    if(num_of_combat_planes == 0 && num_of_energy_bullets == 0){
+      break;
+    }
+    vector<Point> combat_planes;
+    vector<Point> energy_bullets;
     for(int combat_plane_i = 0; combat_plane_i < num_of_combat_planes; combat_plane_i++){
       int x,y;
       scanf("%d %d",&x,&y);
+      combat_planes.push_back(Point(x,y));
     }
     for(int energy_bullet_i = 0; energy_bullet_i < num_of_energy_bullets; energy_bullet_i++){
       int x,y;
       scanf("%d %d",&x,&y);
+      energy_bullets.push_back(Point(x,y));
     }
+
+    int sum = 0;
+    for(int combat_plane_i = 0; combat_plane_i < num_of_combat_planes; combat_plane_i++){
+      for(int energy_bullet_i = 0; energy_bullet_i < num_of_energy_bullets; energy_bullet_i++){
+	if(combat_planes[combat_plane_i].include(energy_bullets[energy_bullet_i],radius)){
+	  sum++;
+	}
+      }
+    }
+
+    printf("%d\n",sum);
   }
 }
