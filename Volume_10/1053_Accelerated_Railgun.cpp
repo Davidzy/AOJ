217ff8131bbffe1ba9a14380df326f6946034de4
titsuki <titsuki@cpan.org>
2015-09-27 14:09:28 +0900

    Add input portion

---
+++ Volume_10/1053_Accelerated_Railgun.cpp
@@ -0,0 +1,42 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  double limit_distance;
+  while(~scanf("%lf",&limit_distance)){
+    if(limit_distance < EPS) break;
+    double px,py;
+    double vx,vy;
+    scanf("%lf %lf",&px,&py);
+    scanf("%lf %lf",&vx,&vy);
+  }
+}
