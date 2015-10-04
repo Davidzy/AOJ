42d274d00e0c366e7b114cfddf31fb3989426423
titsuki <titsuki@cpan.org>
2015-10-04 20:30:31 +0900

    update connect method

--- Volume_23/2307_Palindrome_Generator.cpp
+++ Volume_23/2307_Palindrome_Generator.cpp
@@ -30,22 +30,69 @@ const static int ty[] = {-1,-1,0,1,1,1,0,-1};
  
 static const double EPS = 1e-6;
 
+int dp[101][101][1001];
+
+bool is_palindrome(const string& str){
+  for(int i = 0; i < str.size() / 2; i++){
+    if(str[i] != str[str.size() - i - 1]){
+      return false;
+    }
+  }
+  return true;
+}
+
+void connect(const vector<string>& words,
+	     vector<int> forward_connection[101],
+	     vector<int> backward_connection[101],
+	     int word_i, int word_j, int remaining){
+  // add to left side
+  if(remaining > 0){
+    for(int i = 0; i < backward_connection[word_i].size(); i ++){
+      string front = words[word_j];
+      string rear = words[backward_connection[word_i][i]];
+
+      string other_side = front.substr(front.size() - remaining,remaining);
+      string add_side = rear.substr(rear.size() - remaining,remaining);
+      cout << is_palindrome(add_side + other_side) << endl;
+    }
+  }
+  else {
+    for(int i = 0; i < forward_connection[word_j].size(); i ++){
+      string front = words[forward_connection[word_j][i]];
+      string rear = words[word_i];
+
+      string other_side = front.substr(front.size() - remaining,remaining);
+      string add_side = rear.substr(rear.size() - remaining,remaining);
+      cout << is_palindrome(add_side + other_side) << endl;
+    }
+  }
+}
+
 int main(){
   int num_of_words;
   int num_of_pairs;
   while(~scanf("%d %d",&num_of_words,&num_of_pairs)){
+    memset(dp,-1,sizeof(dp));
+
     vector<string> words;
     for(int word_i = 0; word_i < num_of_words; word_i++){
       string word;
       cin >> word;
       words.push_back(word);
     }
-
-    bool has_path[101][101] = {};
+    
+    vector<int> forward_connection[101];
+    vector<int> backward_connection[101];
     for(int pair_i = 0; pair_i < num_of_pairs; pair_i++){
       int from, to;
       scanf("%d %d",&from,&to);
-      has_path[from][to] = true;
+      from--; to--;
+      forward_connection[from].push_back(to);
+      backward_connection[to].push_back(from);
+    }
+
+    for(int i = 0; i < words.size(); i++){
+      
     }
   }
 }
