ec9c08fb3a32d83e5db3e5985aa6cfc25b3a386d
titsuki <titsuki@cpan.org>
2015-11-22 21:21:30 +0900

    solve 2070 First Experience

--- Volume_20/2070/FirstExperience.scala
+++ Volume_20/2070/FirstExperience.scala
@@ -2,7 +2,7 @@ import scala.io.StdIn
 
 object Main {
   def isInvalid(R1: Int, R2: Int):Boolean = {
-    if(R1 < 0 || R1 >= 10000){
+    if(R1 < 0 || R1 >= 10000 || R2 < 0 || R2 >= 10000){
       return true
     }
     return false
@@ -17,64 +17,54 @@ object Main {
     }
   }
   def solve(line: String){
-    line match {
-      case line: String => {
-        val digit = """(\d+)""".r
-        var R1 = 0
-        var R2 = 0
-        var R3 = "+"
-        val tokens = line.split("").toArray
-        for(i <- 0 to tokens.length - 1){
-          val x = tokens(i).toString
-          x match {
-            case digit(x) => {
-              R2 = 10 * R2 + x.toInt
+    val digit = """(\d+)""".r
+    var R1 = 0
+    var R2 = 0
+    var R3 = "+"
+    val tokens = line.split("").toArray
+    for(i <- 0 to tokens.length - 1){
+      val x = tokens(i).toString
+      x match {
+        case digit(x) => {
+          R2 = 10 * R2 + x.toInt
+        }
+        case "+"|"-"|"*" => {
+          R3 match {
+            case "+" => {
+              R1 = R1 + R2
             }
-            case "+"|"-"|"*" => {
-              R3 match {
-                case "+" => {
-                  R1 = R1 + R2
-                }
-                case "-" => {
-                  R1 = R1 - R2
-                }
-                case "*" => {
-                  R1 = R1 * R2
-                }
-              }
-              R2 = 0
-              R3 = x
-              if(isInvalid(R1,R2)){
-                println("E")
-                return
-              }
+            case "-" => {
+              R1 = R1 - R2
             }
-            case "=" => {
-              R3 match {
-                case "+" => {
-                  R1 = R1 + R2
-                  R2 = 0
-                }
-                case "-" => {
-                  R1 = R1 - R2
-                  R2 = 0
-                }
-                case "*" => {
-                  R1 = R1 * R2
-                  R2 = 0
-                }
-              }
-              printResult(R1,R2)
-              return
-            }
-            case _ => {
-              return
+            case "*" => {
+              R1 = R1 * R2
             }
           }
+          if(isInvalid(R1,R2)){
+            println("E")
+            return
+          }
+          R2 = 0
+          R3 = x
         }
-      }
-      case _ => {
-        return
+        case "=" => {
+          R3 match {
+            case "+" => {
+              R1 = R1 + R2
+              R2 = 0
+            }
+            case "-" => {
+              R1 = R1 - R2
+              R2 = 0
+            }
+            case "*" => {
+              R1 = R1 * R2
+              R2 = 0
+            }
+          }
+          printResult(R1,R2)
+          return
+        }
       }
     }
   }
