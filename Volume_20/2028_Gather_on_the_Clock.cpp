b73c82ea5d54b3e4e850ac7897de97cfb0c7d031
titsuki <titsuki@cpan.org>
2015-12-16 23:44:04 +0900

    Add input portion

---
+++ Volume_20/2028_Gather_on_the_Clock.cpp
@@ -0,0 +1,44 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <complex>
+#include <assert.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair <int,int> P;
+
+static const double EPS = 1e-12;
+
+static const int tx[] = {+0,+1,+0,-1};
+static const int ty[] = {-1,+0,+1,+0};
+
+int main(){
+  int num_of_tests;
+  while(~scanf("%d",&num_of_tests)){
+    for(int test_i = 0; test_i < num_of_tests; test_i++){
+      int num_of_cards;
+      scanf("%d",&num_of_cards);
+      for(int card_i = 0; card_i < num_of_cards; card_i++){
+	int card;
+	scanf("%d",&card);
+      }
+    }
+  }
+}
